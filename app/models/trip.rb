1fd30b0678a2eee66199208456b789f9d8ebbc16
mrudzki <michal.rudzki@akra.net>
2021-08-21 11:43:28 +0200

    Add trip non-zero distance validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,10 @@
 class Trip < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
+  validate :addresses_validation
+
+  private
+
+  def addresses_validation
+    errors.add(:destination_address, "cannot be the same as start address") if start_address == destination_address
+  end
 end
