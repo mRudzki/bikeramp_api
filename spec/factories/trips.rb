5d58240bd015789e8b76103ff1ee5716f1332c4c
mrudzki <michal.rudzki@akra.net>
2021-08-21 11:41:05 +0200

    Create basic trip factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,5 +1,10 @@
+require 'faker'
+
 FactoryBot.define do
   factory :trip do
-    
+    start_address { Faker::Address.full_address }
+    destination_address { Faker::Address.full_address }
+    price { 20 }
+    date { Date.today }
   end
 end
