52c538fdc425e7051178f39d5b8370f269c5693b
mrudzki <michal.rudzki@akra.net>
2021-08-21 13:08:09 +0200

    Add distance column to the trip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_082848) do
+ActiveRecord::Schema.define(version: 2021_08_21_110041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_08_21_082848) do
     t.date "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "distance", precision: 3
     t.index ["date"], name: "index_trips_on_date"
   end
 
