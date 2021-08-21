20effe0d2aac4309ebea9bce76412e96a8ab8710
mrudzki <michal.rudzki@akra.net>
2021-08-21 13:54:38 +0200

    Fix precision values in DB Why this changes? I messed up confusing precision with scale

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_110041) do
+ActiveRecord::Schema.define(version: 2021_08_21_114020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 2021_08_21_110041) do
   create_table "trips", force: :cascade do |t|
     t.string "start_address", null: false
     t.string "destination_address", null: false
-    t.decimal "price", precision: 2, null: false
+    t.decimal "price", precision: 8, scale: 2, null: false
     t.date "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "distance", precision: 3
+    t.decimal "distance", precision: 8, scale: 3
     t.index ["date"], name: "index_trips_on_date"
   end
 
