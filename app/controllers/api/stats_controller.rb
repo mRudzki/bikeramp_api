daefddac6b6f7c913a6762fa8538baa5330194e1
mrudzki <michal.rudzki@akra.net>
2021-08-21 16:56:23 +0200

    Add weekly method

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -1,9 +1,21 @@
 class Api::StatsController < ApplicationController
   def weekly
+    trips = Trip.where('date > ?', beginning_of_week)
 
+    render json: { 'total_distance': trips.sum(:distance), 'total_price': trips.sum(:price) }
   end
 
   def monthly
 
+  end
+
+  private
+
+  def beginning_of_week
+    Date.today.beginning_of_week
+  end
+
+  def beginning_of_month
+    Date.today.beginning_of_month
   end
 end
