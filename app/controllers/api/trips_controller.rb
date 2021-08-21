480f1550316384d347a5c6dbfe6cda5d5b8aee58
mrudzki <michal.rudzki@akra.net>
2021-08-21 12:35:10 +0200

    Add trips controller

---
+++ app/controllers/api/trips_controller.rb
@@ -0,0 +1,14 @@
+class Api::TripsController < ApplicationController
+  def create
+    trip = Trip.new(trip_params)
+    if trip.save
+      render json: trip
+    else
+      render json: { errors: trip.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def trip_params
+    params.require(:trip).permit(:price, :start_address, :destination_address, :date)
+  end
+end
