04c49e6be8804ef8872bb9a2e923589d58514840
mrudzki <michal.rudzki@akra.net>
2021-08-21 15:50:14 +0200

    Add spec check if sidekiq changes data

--- spec/requests/api/trips_spec.rb
+++ spec/requests/api/trips_spec.rb
@@ -1,27 +1,42 @@
 require 'rails_helper'
+require 'sidekiq/testing'
 
 RSpec.describe "Api::Trips", type: :request do
   describe "POST /api/trips" do
-    it "creates record for valid input" do
-      trip_params = {
+    context "when input is valid" do
+      let(:trip_params) do
+        {
           start_address: Faker::Address.full_address,
           destination_address: Faker::Address.full_address,
           price: 17.45,
           date: Date.today
         }
+      end
+      it "creates record" do
+        expect { post api_trips_path, params: { trip: trip_params }, as: :json }.to change(Trip, :count).by(1)
+        expect(response.status).to eq(200)
+      end
 
-      expect { post api_trips_path, params: { trip: trip_params }, as: :json }.to change(Trip, :count).by(1)
-      expect(response.status).to eq(200)
+      it "queues sidekiq worker" do
+        expect { post api_trips_path, params: { trip: trip_params }, as: :json }.to change { DistanceCalculatorWorker.jobs.size }.by(1)
+      end
+
+      it "allows sidekiq to change reocrd" do
+        Sidekiq::Testing.inline! do
+          post api_trips_path, params: { trip: trip_params }, as: :json
+
+          expect(Trip.last.distance).not_to be_nil
+        end
+      end
     end
 
     it "returns errors on invalid record" do
-
-        trip_params = {
-          start_address: Faker::Address.full_address,
-          destination_address: Faker::Address.full_address,
-          price: -20,
-          date: Date.today
-        }
+      trip_params = {
+        start_address: Faker::Address.full_address,
+        destination_address: Faker::Address.full_address,
+        price: -20,
+        date: Date.today
+      }
       aggregate_failures do
         expect { post api_trips_path, params: { trip: trip_params }, as: :json }.not_to change(Trip, :count)
         expect(response.status).to eq(422)
