5f3d28a6662e458da016d9a76c837810eb1343dc
mrudzki <michal.rudzki@akra.net>
2021-08-21 16:26:19 +0200

    Add distance to trip factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     start_address { Faker::Address.full_address }
     destination_address { Faker::Address.full_address }
     price { 20 }
+    distance { 100 }
     date { Date.today }
   end
 end
