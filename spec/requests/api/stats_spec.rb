36221f0632896b37967928c2edf443173303fa41
mrudzki <michal.rudzki@akra.net>
2021-08-21 16:26:58 +0200

    Move stats requests spec to right folder

---
+++ spec/requests/api/stats_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Api::Stats", type: :request do
+  describe "GET /weekly" do
+    it "returns sum of all trip distances of current week" do
+
+    end
+
+    it "returns sum of all trip distances of current week" do
+
+    end
+  end
+end
