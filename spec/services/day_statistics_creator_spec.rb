9ff506cb032e79d591b8a1627537594ea4f0ad8a
mrudzki <michal.rudzki@akra.net>
2021-08-22 14:25:20 +0200

    Add basic specs to day_statistics

---
+++ spec/services/day_statistics_creator_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe DayStatisticsCreator do
+  describe "#call" do
+    let(:day) do
+      trip1 = create :trip, distance: 2.048, price: 32.64, date: Date.today
+      trip2 = create :trip, distance: 1.024, price: 8.16, date: Date.today
+      [
+        Date.today,
+        [
+          trip1,
+          trip2
+        ]
+      ]
+    end
+    it "perform all calculations correctly" do
+      aggregate_failures do
+        expect(described_class.new(day).call[:total_distance]).to eq('3.072 km')
+        expect(described_class.new(day).call[:avg_ride]).to eq('1.536 km')
+        expect(described_class.new(day).call[:avg_price]).to eq('20.40 PLN')
+      end
+    end
+  end
+end
