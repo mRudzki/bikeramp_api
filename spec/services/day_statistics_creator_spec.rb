dc07a7a3cedb5cebd9d30455b428285520caac2d
mrudzki <michal.rudzki@akra.net>
2021-08-22 14:28:19 +0200

    Add edge case found in previous manual tests When day has at least one nil distance and one normal distance: undefined method `+' for nil:NilClass in app/services/day_statistics_creator.rb:22 appears

--- spec/services/day_statistics_creator_spec.rb
+++ spec/services/day_statistics_creator_spec.rb
@@ -13,11 +13,30 @@ RSpec.describe DayStatisticsCreator do
         ]
       ]
     end
+
     it "perform all calculations correctly" do
       aggregate_failures do
         expect(described_class.new(day).call[:total_distance]).to eq('3.072 km')
         expect(described_class.new(day).call[:avg_ride]).to eq('1.536 km')
         expect(described_class.new(day).call[:avg_price]).to eq('20.40 PLN')
+      end
+    end
+
+    context "when one of daily distances is nil" do
+      let(:day) do
+        trip_with_nil_distance = create :trip, distance: nil, date: Date.today
+        trip = create :trip, distance: 2.048, date: Date.today
+        [
+          Date.today,
+          [
+            trip_with_nil_distance,
+            trip
+          ]
+        ]
+      end
+
+      it "perform distance calculations correctly" do
+        expect(described_class.new(day).call[:total_distance]).to eq('2.048 km')
       end
     end
   end
