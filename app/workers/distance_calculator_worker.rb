5a194f7ebf0aa480f4a05cfdc12f7f62c1eda573
mRudzki <52656477+mRudzki@users.noreply.github.com>
2021-08-22 15:27:33 +0200

    Refactor distance calculator worker (#5)

--- app/workers/distance_calculator_worker.rb
+++ app/workers/distance_calculator_worker.rb
@@ -5,13 +5,14 @@ class DistanceCalculatorWorker
   def perform(trip_id)
     trip = Trip.find(trip_id)
     request_address = "http://www.mapquestapi.com/directions/v2/route"
-    form = {
+    # details about API https://developer.mapquest.com/documentation/directions-api/
+    params = {
       key: ENV.fetch("MAP_QUEST_KEY"),
       from: trip.start_address,
       to: trip.destination_address,
       unit: 'k' # k-kilometers, default unit is mile
     }
-    response = HTTP.get(request_address, params: form)
+    response = HTTP.get(request_address, params: params)
 
     raise(StandardError.new, "Wrong data or API key please review them") if response.status != 200
 
